29c74f1e58fe7cc9cc513d508f6050a275a39d3b
Ryan Mathisen and Tracie Weitzman <ryan@>
2016-10-18 10:39:17 -0700

    add highlight paragraph toggle

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  // $("body").click(function() {
+  //   $("#myClass").removeClass();
+  // });
+  $("#myClass").click(function() {
+    $("#myClass").toggleClass("pHighlight");
+  });
+  $("#myClass2").click(function() {
+    $("#myClass2").toggleClass("pHighlight");
+  });
+  $("#myClass3").click(function() {
+    $("#myClass3").toggleClass("pHighlight");
+  });
+});
