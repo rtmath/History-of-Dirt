4f02d1ae063b748b9d018aedc0de76d4aa855492
Eric Raetz and Ryan Mathisen <eric@>
2016-10-24 10:16:54 -0700

    refactor the code to use a forEach loop for the click functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
-  // $("body").click(function() {
-  //   $("#myClass").removeClass();
+  var myElems = ["#myClass", "#myClass2", "#myClass3"];
+  myElems.forEach(function(elem) {
+    $(elem).click(function() {
+      $(elem).toggleClass("pHighlight");
+    });
+  });
+
+  // $("#myClass").click(function() {
+  //   $("#myClass").toggleClass("pHighlight");
   // });
-  $("#myClass").click(function() {
-    $("#myClass").toggleClass("pHighlight");
-  });
-  $("#myClass2").click(function() {
-    $("#myClass2").toggleClass("pHighlight");
-  });
-  $("#myClass3").click(function() {
-    $("#myClass3").toggleClass("pHighlight");
-  });
+  // $("#myClass2").click(function() {
+  //   $("#myClass2").toggleClass("pHighlight");
+  // });
+  // $("#myClass3").click(function() {
+  //   $("#myClass3").toggleClass("pHighlight");
+  // });
 });
